48cec6f5ea86c6fbf535de4b0a8b996c951c5a5c
Katie Gail <katherine.nolde@gmail.com>
2020-06-24 10:07:39 -0400

    server setup for heroku db

--- connection.js
+++ connection.js
@@ -1,11 +1,17 @@
 const { Pool } = require("pg");
-const credentials = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    //CHANGE DATABASE NAME BASED ON YOUR CREATED ONE
-    database: "FinalProject",
-    ssl: false
+try {
+  // When not running via Heroku, this will load the .env file.
+  require("dotenv").config();
+} catch (e) {
+  // When running with Heroku, dotenv doesn't need to be available.
+}
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+  connectionString: connectionString,
+  //this is what made it work
+  //https://help.heroku.com/MDM23G46/why-am-i-getting-an-error-when-i-upgrade-to-pg-8
+  ssl: connectionString.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false },
 });
-module.exports = credentials;
+module.exports = pool;
